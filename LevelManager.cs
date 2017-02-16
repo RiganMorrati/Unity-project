524998624ecb6d9ff1a45884594b433f6dc79c01
RiganMorrati <riganmorati@gmail.com>
2017-02-16 16:14:12 +0300

    Add files via upload

---
+++ LevelManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public List<GameObject> lvls;
+    public Transform canvas;
+    public WinPanel winPanel;
+    public GameObject gameCompletedPanel;
+
+    public static LevelManager lManager;
+    void Start()
+    {
+        lManager = this;
+        CurrentLevel = 0;
+    }
+
+    GameObject currentLevel = null;
+    sbyte currentLevelNumber = -1;
+    public sbyte CurrentLevel
+    {
+        get
+        {
+            return currentLevelNumber;
+        }
+        set
+        {
+            if(value < 0 || value > lvls.Count || value == currentLevelNumber)
+                return;
+            winPanel.SetActive(false);
+            if (currentLevel != null)
+                Destroy(currentLevel);
+            if (value == lvls.Count)
+            {
+                WinScenario();
+                return;
+            }
+            currentLevel = Instantiate(lvls[currentLevelNumber = value]);
+            currentLevel.transform.SetParent(canvas);
+            currentLevel.transform.SetAsFirstSibling();
+            currentLevel.transform.name = "Level" + (currentLevelNumber + 1);
+        }
+    }
+
+    void WinScenario()
+    {
+        gameCompletedPanel.SetActive(true);
+    }
+}
